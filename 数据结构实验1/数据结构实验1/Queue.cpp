750f95e69e4951de1a1646818ea1c9aae1429dbb
Peterliang <ncuyanping666@126.com>
2020-11-20 08:29:36 +0800

    rename file and delete extra directory

--- 数据结构实验1/数据结构实验1/Queue.cpp
+++
@@ -1,7 +0,0 @@
-#include "head.h"
-bool InitQueue(LinkQueue &Q) {  //初始化普通队列
-	Q.front = Q.rear = (QueuePtr)malloc(sizeof(QNode));
-	if (!Q.front) return false;
-	Q.front->next = NULL;
-	return true;
-}
